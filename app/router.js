db7d560a64b72023df91e111d15b9510b7b75f14
Jeff Keen <jeff@keen.me>
2018-08-27 16:26:10 -0500

    Fix some dependencies, add analytics

--- app/router.js
+++ app/router.js
@@ -1,13 +1,36 @@
 import EmberRouter from "@ember/routing/router";
 import config from "./config/environment";
 import { inject } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
+import { get } from '@ember/object';
+
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
   headData: inject(),
+  metrics: inject(),
+  fastboot: inject(),
+
   setTitle(title) {
     this.get('headData').set('title', title);
+  },
+
+  didTransition() {
+    this._super(...arguments);
+    this._trackPage();
+  },
+
+  _trackPage() {
+    if (!get(this, 'fastboot.isFastBoot')) {
+      scheduleOnce('afterRender', this, () => {
+        const page = this.get('url');
+        const title = this.getWithDefault('currentRouteName', 'unknown');
+
+        get(this, 'metrics').trackPage({ page, title });
+      });
+    }
   }
+
 });
 
 Router.map(function() {
--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,24 @@ module.exports = function(environment) {
      'Ledger'
     ],
 
+    metricsAdapters: [
+      {
+        name: 'GoogleAnalytics',
+        environments: ['development', 'production'],
+        config: {
+          id: 'UA-719104-13',
+          // Use `analytics_debug.js` in development
+          debug: environment === 'development',
+          // Use verbose tracing of GA events
+          trace: environment === 'development',
+          // Ensure development env hits aren't sent to GA
+          sendHitTask: environment !== 'development',
+          // Specify Google Analytics plugins
+          require: ['ecommerce']
+        }
+      }
+    ],
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
